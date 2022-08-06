0ee4b6697bec695c9916646fae08b6389394a01d
Grandelwald <artjr97@naver.com>
2022-08-06 13:39:32 +0900

    [feat] 컨트롤러에 Adapter 추가하여 뷰와 연동

---
+++ app/src/main/java/com/example/movieapplicationpractice/BindingAdapters.kt
@@ -0,0 +1,15 @@
+package com.example.movieapplicationpractice
+
+import android.widget.ImageView
+import androidx.databinding.BindingAdapter
+import com.bumptech.glide.Glide
+
+object BindingAdapters {
+    @JvmStatic
+    @BindingAdapter("imageUrl")
+    fun loadImage(view: ImageView, url: String) {
+        Glide.with(view.context)
+            .load(url)
+            .into(view)
+    }
+}
--- app/src/main/java/com/example/movieapplicationpractice/MainActivity.kt
+++ app/src/main/java/com/example/movieapplicationpractice/MainActivity.kt
@@ -6,28 +6,9 @@ import android.os.Bundle
 import androidx.fragment.app.FragmentTransaction
 import com.example.movieapplicationpractice.databinding.ActivityMainBinding
 
-
-//var mockDate: Array<Movie> = [
-//    Movie(imageURL: 'https://fuck.png', title: 'fuck'),
-//
-//Movie(imageURL: 'https://fuck.png', title: 'fuck'),
-//Movie(imageURL: 'https://fuck.png', title: 'fuck'),
-//Movie(imageURL: 'https://fuck.png', title: 'fuck'),
-//Movie(imageURL: 'https://fuck.png', title: 'fuck'),
-//Movie(imageURL: 'https://fuck.png', title: 'fuck'),
-//Movie(imageURL: 'https://fuck.png', title: 'fuck'),
-//
-//]
-
-var Movies : ArrayList<Movie> = arrayListOf(
-    Movie("https://movie-phinf.pstatic.net/20220727_209/1658912961873lVd2W_JPEG/movie_image.jpg?type=f125","한산 : 용의 출현"),
-    Movie("https://movie-phinf.pstatic.net/20220720_283/1658284839003UzxoT_JPEG/movie_image.jpg?type=f125","미니언즈 2"),
-    Movie("https://movie-phinf.pstatic.net/20220509_176/1652081912471yhg3N_JPEG/movie_image.jpg?type=m203_290_2","탑건 : 메버릭"),
-    Movie("https://movie-phinf.pstatic.net/20220708_75/16572722362230AyHS_JPEG/movie_image.jpg?type=f125","외계+인 1부")
-)
-
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
+    lateinit var adapter: MainRvAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -39,5 +20,14 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this, DetailActivity::class.java)
             startActivity(intent)
         }
+
+        initAdapter()
+    }
+
+    private fun initAdapter(){
+        adapter = MainRvAdapter()
+        binding.rvMainRecycler.adapter = adapter
+
+        adapter.notifyDataSetChanged()
     }
 }
